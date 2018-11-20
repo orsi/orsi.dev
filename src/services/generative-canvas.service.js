258a456d480268a2fb676f7f99a31f9f867eb422
Jonathon Orsi <jonathon.orsi@gmail.com>
2018-11-19 21:18:41 -0500

    integrate cellular automata into canvas for splash

--- src/services/generative-canvas.service.js
+++ src/services/generative-canvas.service.js
@@ -1,25 +1,109 @@
-let context, running = false, fps = 1000 / 16, cellWidth, cellHeight, grid, gridLength, gridWidth, iteration = 0;
-const hueMax = 360,
+import Automata from '../utils/automata.js';
+
+let context,
+    running = false,
+    fps = 60,
+    tickTime = 1000 / fps,
+    grid,
+    gridWidth,
+    gridHeight,
+    automata,
+    iteration = 0;
+const cellWidth = 25,
+    cellHeight = 25,
+    hueMax = 360,
     hueMin = 0,
-    saturationMax = 80,
-    saturationMin = 20,
-    lightMax = 35,
-    lightMin = 20;
+    saturationMax = 70,
+    saturationMin = 60,
+    lightMax = 50,
+    lightMin = 1;
+let now, then = Date.now(), delta;
+function tick() {
+    if (running) requestAnimationFrame(tick);
+    now = Date.now();
+    delta = now - then;
+    if (delta > tickTime) {
+        then = now - (delta % tickTime);
+        update();
+        draw();
+    }
+}
+function update() {
+    iteration++;
+    // update is run 60 fps, update automata every 4 frames
+    if (iteration % (fps / 1) === 0) {
+        if (automata.isEmpty || automata.isStale) {
+            automata = new Automata(gridWidth, gridHeight);
+            console.log(automata);
+        } else {
+            automata.next();
+        }
+    }
+
+    for (let i = 0; i < gridWidth; i++) { // row
+        for (let j = 0; j < gridHeight; j++) { // column
+            const cell = grid[i][j];
+
+            cell.hue = cell.hue + (1 * cell.hueDirection);
+            if (cell.hue >= hueMax) cell.hueDirection = -1;
+            if (cell.hue <= hueMin) cell.hueDirection = 1;
+
+            cell.saturation = cell.saturation + (1 * cell.saturationDirection);
+            if (cell.saturation >= saturationMax) cell.saturationDirection = -1;
+            if (cell.saturation <= saturationMin) cell.saturationDirection = 1;
+
+            cell.light = cell.light + (1 * cell.lightDirection);
+            if (cell.light >= lightMax) cell.lightDirection = -1;
+            if (cell.light <= lightMin) cell.lightDirection = 1;
+
+            cell.alive = automata.getGrid()[i][j];
+        }
+    }
+}
+function draw() {
+    // clear canvas
+    const canvas = context.canvas;
+    context.fillStyle = '#141414';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // draw
+    for (let i = 0; i < gridWidth; i++) {
+        for (let j = 0; j < gridHeight; j++) {
+            const cell = grid[i][j];
+            if (cell.alive) {
+                context.fillStyle = `hsl(${cell.hue},${cell.saturation}%,${cell.light}%)`;
+                context.fillRect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
+            }
+        }
+    }
+}
+
 const service = {
     init: (ctx) => {
         context = ctx;
         const canvas = context.canvas;
-        context.canvas.width = context.canvas.clientWidth;
-        context.canvas.height = context.canvas.clientHeight;
-        cellWidth = cellHeight = 10;
+        canvas.width = context.canvas.clientWidth;
+        canvas.height = context.canvas.clientHeight;
+
+        window.addEventListener('resize', () => {
+            canvas.width = context.width = window.innerWidth;
+            canvas.height = context.height = window.innerHeight;
+        });
 
         // build cell grid
         grid = [];
-        gridLength = Math.floor(canvas.height / cellHeight) + 1; // +1 to draw past edge of canvas
-        gridWidth = Math.floor(canvas.width / cellWidth) + 1;
-        for (let i = 0; i < gridLength; i++) { // row
+        gridWidth = Math.floor(canvas.width / cellWidth) + 1; // +1 to draw past edge of canvas
+        gridHeight = Math.floor(canvas.height / cellHeight) + 1;
+
+        // initiate automata
+        automata = new Automata(gridWidth, gridHeight);
+
+        console.log(automata);
+
+        // write out grid cells
+        for (let i = 0; i < gridWidth; i++) { // row
             grid[i] = [];
-            for (let j = 0; j < gridWidth; j++) { // column
+            for (let j = 0; j < gridHeight; j++) { // column
                 const hueRange = hueMax - hueMin;
                 const hue = Math.floor(Math.random() * hueRange) + hueMin;
                 const saturationRange = saturationMax - saturationMin;
@@ -33,6 +117,7 @@ const service = {
                     saturationDirection: 1,
                     light: light,
                     lightDirection: 1,
+                    alive: automata.getGrid()[i][j]
                 };
             }
         }
@@ -47,51 +132,3 @@ const service = {
     }
 }
 export default service;
-
-let now, then = Date.now(), delta;
-function tick() {
-    if (running) requestAnimationFrame(tick);
-    now = Date.now();
-    delta = now - then;
-    if (delta > fps) {
-        then = now - (delta % fps);
-        update();
-        draw();
-    }
-}
-function update() {
-    iteration++;
-    for (let i = 0; i < gridLength; i++) { // row
-        for (let j = 0; j < gridWidth; j++) { // column
-            const cell = grid[i][j];
-            cell.hue = cell.hue + (1 * cell.hueDirection);
-            if (cell.hue >= hueMax) cell.hueDirection = -1;
-            if (cell.hue <= hueMin) cell.hueDirection = 1;
-
-            cell.saturation = cell.saturation + (1 * cell.saturationDirection);
-            if (cell.saturation >= saturationMax) cell.saturationDirection = -1;
-            if (cell.saturation <= saturationMin) cell.saturationDirection = 1;
-
-            cell.light = cell.light + (1 * cell.lightDirection);
-            if (cell.light >= lightMax) cell.lightDirection = -1;
-            if (cell.light <= lightMin) cell.lightDirection = 1;
-        }
-    }
-}
-function draw() {
-    // clear canvas
-    const canvas = context.canvas;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    // draw
-    for (let i = 0; i < gridLength; i++) {
-        for (let j = 0; j < gridWidth; j++) {
-            const cell = grid[i][j];
-            const hue = cell.hue;
-            const saturation = cell.saturation;
-            const light = cell.light;
-            context.fillStyle = `hsl(${hue},${saturation}%,${light}%)`;
-            context.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
-        }
-    }
-}
---
+++ src/utils/automata.js
@@ -0,0 +1,93 @@
+export default class Automata {
+    constructor(width, height, options) {
+        this.width = width;
+        this.height = height;
+        this.options = options || {
+            aliveProbability: .5
+        };
+        this.isEmpty = false;
+        this.isStale = false;
+        this.lastCellCounts = [];
+        this.cellCount = 0;
+        this.setup(this.options);
+    }
+    setup (options) {
+        this.grid = [];
+        for (let i = 0; i < this.width; i++) {
+            this.grid[i] = [];
+            for (let j = 0; j < this.height; j++) {
+                this.grid[i][j] = Math.random() < options.aliveProbability;
+                if (this.grid[i][j]) this.cellCount++;
+            }
+        }
+        this.lastCellCounts.push(this.cellCount);
+    }
+    getGrid =  () => this.grid;
+    next =  () => {
+        let newGrid = [];
+        let isEmpty = true; // optimizes checking if grid is completely empty
+        this.cellCount = 0;
+
+        for (let i = 0; i < this.width; i++) {
+            newGrid[i] = [];
+            for (let j = 0; j < this.height; j++) {
+                const isAlive = this.grid[i][j];
+                const aliveNeighbours = this.countAliveNeighbours(i, j);
+
+                // rules of survivability, modified from Conway's game to increase the
+                // amount of alive cells on grid
+                if (isAlive &&
+                    (aliveNeighbours <= 0 || aliveNeighbours >= 6)) {
+                    newGrid[i][j] = false;
+                } else if (aliveNeighbours >= 3 && aliveNeighbours <= 6) {
+                    newGrid[i][j] = true;
+                    if (isEmpty) isEmpty = false;
+                }
+                if (newGrid[i][j]) this.cellCount++;
+            }
+        }
+
+        // if isEmpty is still true afterwards, this grid is dead
+        this.isEmpty = isEmpty;
+
+        // check if grid is stale
+        let sameCount = 0;
+        for (let i = 0; i < 10; i++) {
+            const index = this.lastCellCounts.length - 1 - i;
+            if (this.lastCellCounts[index] === this.cellCount) sameCount++;
+        }
+        // if atleast 5 iterations were the same, consider stale
+        if (sameCount >= 5) {
+            this.isStale = true;
+        }
+
+        // save cellCount
+        this.lastCellCounts.push(this.cellCount);
+        if (this.lastCellCounts.length > 10) {
+            this.lastCellCounts.shift();
+        }
+
+        this.grid = newGrid;
+    }
+    countAliveNeighbours = (x, y) => {
+        let aliveCellsCount = 0;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                let ix = x + i;
+                let jy = y + j;
+
+                // normalize indexes to wrap around grid
+                if (ix < 0) ix = this.width - 1;
+                if (ix >= this.width) ix = 0;
+                if (jy < 0) jy = this.height - 1;
+                if (jy >= this.height) jy = 0;
+
+                // don't check same cell
+                if (ix === x && jy === y) continue;
+
+                if (this.grid[ix][jy]) aliveCellsCount++;
+            }
+        }
+        return aliveCellsCount;
+    }
+}

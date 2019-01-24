c455faaca8074785f31b963d177fa10e31c7e659
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-01-23 21:53:53 -0500

    integrate splash into home component and remove splash

--- src/app.component.js
+++ src/app.component.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './app.component.css';
 import * as FaviconService from './services/favicon.service.js';
+import { BackButtonComponent } from './components/back-button.component.js';
 import HomeViewComponent from './views/home/home.component.js';
 import AboutViewComponent from './views/about/about.component.js';
 import WorkViewComponent from './views/work/work.component.js';
@@ -16,15 +17,18 @@ class AppComponent extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div className="app">
-          <Route exact path="/" component={HomeViewComponent} />
-          <Route exact path="/about" component={AboutViewComponent} />
-          <Route exact path="/work" component={WorkViewComponent} />
-          <Route exact path="/projects" component={ProjectsViewComponent} />
-          <Route exact path="/resume" component={ResumeViewComponent} />
+          <BackButtonComponent></BackButtonComponent>
+          <Switch>
+            <Route exact path="/" component={HomeViewComponent} />
+            <Route exact path="/about" component={AboutViewComponent} />
+            <Route exact path="/work" component={WorkViewComponent} />
+            <Route exact path="/projects" component={ProjectsViewComponent} />
+            <Route exact path="/resume" component={ResumeViewComponent} />
+          </Switch>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
--- src/views/home/home.component.js
+++ src/views/home/home.component.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import './home.component.css';
 
-import Splash from '../../components/splash.component.js';
+import { Link } from 'react-router-dom';
 
 class HomeViewComponent extends Component {
-
   componentDidMount() {
   }
-
   render() {
     return (
       <div className="home-view">
-        <Splash />
+        <header className="home-header">
+              <div className="flex justify-content-center align-items-center flex-column">
+                  <div className="">
+                      <h1>
+                          <span className="jonathon">Jonathon</span>
+                          <span className="orsi">Orsi</span>
+                      </h1>
+                  </div>
+                  <div className="">
+                      <h2>
+                          Full-stack Developer
+                      </h2>
+                  </div>
+
+                  <div className="view-links">
+                    <ul className="view-links-list list-unstyled">
+                        <li className="view-links-list-item"><Link to="/about">About</Link></li>
+                        <li className="view-links-list-item"><Link to="/work">Work</Link></li>
+                        <li className="view-links-list-item"><Link to="/projects">Projects</Link></li>
+                        <li className="view-links-list-item"><Link to="/resume">Resume</Link></li>
+                    </ul>
+                  </div>
+              </div>
+          </header>
       </div>
     );
   }

32ec3ea7e60c48fbf74f4826800b98f6551fed6a
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-02-17 18:57:41 -0500

    add flex columns layout classes

--- src/views/about/about.component.js
+++ src/views/about/about.component.js
@@ -4,20 +4,26 @@ import guitar from '../../assets/guitar.jpg';
 
 export default class AboutComponent extends Component {
     render() {
-        return(
+        return (
             <div className="about">
                 <div className="container">
-                    <h2>About</h2>
+                    <div className="row">
+                        <div className="column">
+                            <h2>About</h2>
 
-                    <p>I am a full-stack web developer with a focus on responsive and intuitive user experiences. My current work for Bombardier Aerospace involves digitizing business workflow processes into enterprise web applications.</p>
+                            <p>I am a full-stack web developer with a focus on responsive and intuitive user experiences. My current work for Bombardier Aerospace involves digitizing business workflow processes into enterprise web applications.</p>
 
-                    <div className="about__programming">
-                        <p>I often experiment with <a href="https://www.webcomponents.org/introduction">custom web components</a>, and I have a deep interest in using the browser as a platform for <a href="https://github.com/jorsi/reverie/tree/develop">experimenting</a>.</p>
-                    </div>
+                            <div className="about__programming">
+                                <p>I often experiment with <a href="https://www.webcomponents.org/introduction">custom web components</a>, and I have a deep interest in using the browser as a platform for <a href="https://github.com/jorsi/reverie/tree/develop">experimenting</a>.</p>
+                            </div>
 
-                    <div className="about__music">
-                        <p>When I'm not coding, you can find me <a href="https://www.youtube.com/watch?v=eyLexd5MCCw&feature=youtu.be&t=14">laying down a bassline</a>, <a href="https://www.youtube.com/watch?v=zOHUfhNQsoc&feature=youtu.be&t=1231">shredding the guitar</a> and <a href="http://jojogun.ca">belting a few songs</a>.</p>
-                        <img src={guitar} alt="Jon playing guitar"></img>
+                            <div className="about__music">
+                                <p>When I'm not coding, you can find me <a href="https://www.youtube.com/watch?v=eyLexd5MCCw&feature=youtu.be&t=14">laying down a bassline</a>, <a href="https://www.youtube.com/watch?v=zOHUfhNQsoc&feature=youtu.be&t=1231">shredding the guitar</a> and <a href="http://jojogun.ca">belting a few songs</a>.</p>
+                            </div>
+                        </div>
+                        <div className="column flex align-items-center">
+                            <img src={guitar} alt="Jon playing guitar"></img>
+                        </div>
                     </div>
 
                 </div>

2657156a62dc5c71a34db4cda82d8570d0e28762
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-02-17 18:31:54 -0500

    add zen-html and reverie to projects section

--- src/views/projects/projects.component.js
+++ src/views/projects/projects.component.js
@@ -4,8 +4,8 @@ import imageBeticalFull from '../../assets/work-betical-full.jpg';
 import imageBeticalMobile from '../../assets/work-betical-mobile.jpg';
 import imageHumbleRootsMediaFull from '../../assets/work-humble-roots-media-full.jpg';
 import imageHumbleRootsMediaMobile from '../../assets/work-humble-roots-media-mobile.jpg';
-import imageJoJoGunFull from '../../assets/work-jo-jo-gun-full.jpg';
-import imageJoJoGunMobile from '../../assets/work-jo-jo-gun-mobile.jpg';
+import imageZenHtml from '../../assets/zen-html.png';
+import imageReverie from '../../assets/reverie.png';
 
 export default class ProjectsComponent extends Component {
     render() {
@@ -18,42 +18,38 @@ export default class ProjectsComponent extends Component {
                     <div className="col-10">
 
                         <div className="project-item">
-                            <a className="project-link" href="">
-                                <div className="project-image-container">
-                                    <img className="project-image full" alt="" src={ imageBeticalFull } />
-                                    <img className="project-image mobile" alt="" src={ imageBeticalMobile } />
-                                </div>
-                                <span className="project-screen"></span>
-                                <span className="project-title">
-                                    <span>Betical</span>
-                                </span>
-                            </a>
+                            <h3>Betical</h3>
+
+                            <div className="project-image-container">
+                                <img className="project-image full" alt="" src={ imageBeticalFull } />
+                                <img className="project-image mobile" alt="" src={ imageBeticalMobile } />
+                            </div>
+                            <p>A born-digital paragraph generator composed with remixed typed assemblage-letters by Dani Spinosa of <a href="https://genericpronoun.com/">Generic Pronoun</a>. Dani came to me with a set of typed letters she had created with the idea of a randomly generated "paragraph". Using the letter images with a combination of javascript and jQuery, Betical generates random combinations of letters into words, and words into paragraphs, to give the impression of a familiar language.</p>
                         </div>
 
                         <div className="project-item">
-                            <a className="project-link" href="">
-                                <div className="project-image-container">
-                                    <img className="project-image full" alt="" src={ imageHumbleRootsMediaFull } />
-                                    <img className="project-image mobile" alt="" src={ imageHumbleRootsMediaMobile } />
-                                </div>
-                                <span className="project-screen"></span>
-                                <span className="project-title">
-                                    <span>Humble Roots Media</span>
-                                </span>
-                            </a>
+                            <span>Humble Roots Media</span>
+                            <div className="project-image-container">
+                                <img className="project-image full" alt="" src={ imageHumbleRootsMediaFull } />
+                                <img className="project-image mobile" alt="" src={ imageHumbleRootsMediaMobile } />
+                            </div>
+                            <p>Humble Roots Media, born from Adrian Miller and Myles Herod, is a digital content creator for businesses and corporations. They wanted a redesign of their website to better reflect their business personality and needed someone who could work with them to implement their vision. Built on Wordpress, I worked with both Adrian and Myles on a new theme template and built an api to deliver and display their beautiful cinemagraphs front and center.</p>
                         </div>
 
                         <div className="project-item">
-                            <a className="project-link" href="">
-                                <div className="project-image-container">
-                                    <img className="project-image full" alt="" src={ imageJoJoGunFull } />
-                                    <img className="project-image mobile" alt="" src={ imageJoJoGunMobile } />
-                                </div>
-                                <span className="project-screen"></span>
-                                <span className="project-title">
-                                    <span>Jo Jo Gun & the Bullets</span>
-                                </span>
-                            </a>
+                            <span>Zen HTML</span>
+                            <div className="project-image-container">
+                                <img className="project-image full" alt="" src={ imageZenHtml } />
+                            </div>
+                            <p>Zen HTML is a small, quick, and simple library for creating Custom Web Elements through ES5 tagged string literals. Heavily inspired by <a href="">lit-html</a> and <a href="">hyper-html</a>, Zen HTML is a learning project I've built to better understand new Javascript features, npm, custom web elements, and overall project development, maintainance, and publishing.</p>
+                        </div>
+
+                        <div className="project-item">
+                            <span>Reverie</span>
+                            <div className="project-image-container">
+                                <img className="project-image full" alt="" src={ imageReverie } />
+                            </div>
+                            <p>Reverie is an experiment in every sense. Fascinated by procedural generation and emerging properties, Reverie is my attempt at building a world simulation entirely with Node.js and the browser.</p>
                         </div>
                     </div>
                 </div>

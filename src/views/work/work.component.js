4afc541f1c8bcaa9ade101707458a1c76ab5b5de
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-02-19 19:08:23 -0500

    style work section

--- src/views/work/work.component.js
+++ src/views/work/work.component.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import './work.component.css';
+import { IconContext } from "react-icons";
+import { FiEdit, FiImage, FiCode } from 'react-icons/fi';
 
 export default class WorkComponent extends Component {
     render() {
-        return(
+        return (
             <div className="work">
                 <div className="container">
                     <h2 className="section-title">Work</h2>
-                    <div className="work-item">
-                        <div className="work-item-title">
-                            <span>Full Stack Developer @ <strong>Bombardier Inc.</strong></span>
+                    <div className="row">
+                        <div className="column">
+                            <div className="work__splash flex justify-content-between align-items-center">
+                                <IconContext.Provider value={{ className: "react-icons" }}>
+                                    <FiEdit className="analyze red" />
+                                    <FiImage className="design green" />
+                                    <FiCode className="develop blue" />
+                                </IconContext.Provider>
+                            </div>
                         </div>
-                        <time className="work-item-time">October 2016</time>
-                        <p className="work-item-description">Design and develop enterprise web applications which digitize business processes currently used to build the Global, Q Series, C Series, and Lear aircraft.</p>
-                    </div>
-                    <div className="work-item">
-                        <div className="work-item-title">
-                            <span>Freelance Web Developer</span>
+                        <div className="column">
+                            <div className="work__item">
+                                <div className="work__title">
+                                    <span>Full Stack Developer @ <strong>Bombardier Inc.</strong></span>
+                                </div>
+                                <time className="work__duration">October 2016 &rarr; Present</time>
+                                <p className="work__description">Design and develop enterprise web applications which digitize business processes currently used to build the Global, Q Series, C Series, and Lear aircraft.</p>
+                            </div>
+                            <div className="work__item">
+                                <div className="work__title">
+                                    <span>Freelance Web Developer</span>
+                                </div>
+                                <time className="work__duration">April 2015 &rarr; Present</time>
+                                <p className="work__description">Freelance web development and design work for small-to-medium sized businesses.</p>
+                            </div>
                         </div>
-                        <time className="work-item-time">April 2015</time>
-                        <p className="work-item-description">Freelance web development and design work for small-to-medium sized businesses.</p>
                     </div>
                 </div>
             </div>

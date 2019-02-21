18a9adcd4b32ea2c19f654b79f20f296ea17832c
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-02-20 22:46:38 -0500

    modify projects layout

--- src/views/projects/projects.component.js
+++ src/views/projects/projects.component.js
@@ -18,15 +18,15 @@ export default class ProjectsComponent extends Component {
                                 <a href="#" className="project__visit-button">Visit</a>
                             </div>
                         </div>
-                        <div class="row">
-                            <div className="column w-m-50">
+                        <div className="row">
+                            <div className="column w-m-66">
                                 <p>A born-digital paragraph generator composed with remixed typed assemblage-letters by Dani Spinosa of <a href="https://genericpronoun.com/">Generic Pronoun</a>. Dani came to me with a set of typed letters she had created with the idea of a randomly generated "paragraph". Using the letter images with a combination of javascript and jQuery, Betical generates random combinations of letters into words, and words into paragraphs, to give the impression of a familiar language.</p>
                             </div>
-                            <div className="column w-m-50">
+                            <div className="column w-m-33">
                                 <div className="project__picture__container">
                                     <img src={imageBetical}
                                         alt="Betical"
-                                        className="project__picture project__picture-right" />
+                                        className="project__picture project__picture-right max-w" />
                                 </div>
                             </div>
                         </div>
@@ -40,16 +40,16 @@ export default class ProjectsComponent extends Component {
                             </div>
                         </div>
                         <div className="row">
-                            <div className="column w-m-50">
+                            <div className="column w-m-66">
+                                <p>Humble Roots Media, born from Adrian Miller and Myles Herod, is a digital content creator for businesses and corporations. They wanted a redesign of their website to better reflect their business personality and needed someone who could work with them to implement their vision. Built on Wordpress, I worked with both Adrian and Myles on a new theme template and built an api to deliver and display their beautiful cinemagraphs front and center.</p>
+
+                            </div>
+                            <div className="column w-m-33">
                                 <div className="project__picture__container">
                                     <img src={imageHumbleRootsMedia}
                                         alt="Humble Roots Media"
-                                        className="project__picture project__picture-left" />
+                                        className="project__picture project__picture-right" />
                                 </div>
-                            </div>
-                            <div className="column w-m-50">
-                                <p>Humble Roots Media, born from Adrian Miller and Myles Herod, is a digital content creator for businesses and corporations. They wanted a redesign of their website to better reflect their business personality and needed someone who could work with them to implement their vision. Built on Wordpress, I worked with both Adrian and Myles on a new theme template and built an api to deliver and display their beautiful cinemagraphs front and center.</p>
-
                             </div>
                         </div>
 
@@ -63,12 +63,12 @@ export default class ProjectsComponent extends Component {
                             </div>
                         </div>
                         <div className="row">
-                            <div className="column w-m-50">
+                            <div className="column w-m-66">
                                 <p>Zen HTML is a small, quick, and simple library for creating Custom Web Elements through ES5 tagged string literals. Heavily inspired by <a href="https://lit-html.polymer-project.org/">lit-html</a> and <a href="https://viperhtml.js.org/hyper.html">hyperHTML</a>, Zen HTML is a learning project I've built to better understand new Javascript features, npm, custom web elements, and overall project development, maintainance, and publishing.</p>
                             </div>
-                            <div className="column w-m-50">
+                            <div className="column w-m-33">
                                 <div className="project__picture__container">
-                                    <img className="project__picture project__picture-right" alt="zenHTML" src={imageZenHtml} />
+                                    <img className="project__picture project__picture-right max-w" alt="zenHTML" src={imageZenHtml} />
                                 </div>
                             </div>
                         </div>
@@ -83,13 +83,13 @@ export default class ProjectsComponent extends Component {
                             </div>
                         </div>
                         <div className="row">
-                            <div className="column w-m-50">
-                                <div className="project__picture__container">
-                                    <img className="project__picture project__picture-left" alt="Reverie" src={imageReverie} />
-                                </div>
-                            </div>
-                            <div className="column w-m-50">
+                            <div className="column w-m-66">
                                 <p className="special-margin-bottom">Reverie is an experiment in every sense. Fascinated by procedural generation and emerging properties, Reverie is my attempt at building a world simulation entirely with Node.js and the browser.</p>
+                            </div>
+                            <div className="column w-m-33">
+                                <div className="project__picture__container">
+                                    <img className="project__picture project__picture-right project__picture-reverie" alt="Reverie" src={imageReverie} />
+                                </div>
                             </div>
                         </div>
                     </div>

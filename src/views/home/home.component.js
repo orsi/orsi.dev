b1516c6c5782d4d16f8ddb465af5841f43d29423
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-02-25 23:43:03 -0500

    improve home section style

--- src/views/home/home.component.js
+++ src/views/home/home.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './home.component.css';
-
 import { Link } from 'react-router-dom';
+import { MdHelp, MdWork, MdCode, MdDescription } from 'react-icons/md';
 
 class HomeViewComponent extends Component {
   componentDidMount() {
@@ -14,14 +14,34 @@ class HomeViewComponent extends Component {
           <span className="orsi">Orsi</span>
         </h1>
         <h2 className="text-center">
-            Full-stack Developer
+            Web Developer
         </h2>
-        <nav className="home__nav w-100 text-center">
+        <nav className="home__nav">
           <ul className="home__links-list list-unstyled">
-              <li className="home__links-item"><Link to="/about">About</Link></li>
-              <li className="home__links-item"><Link to="/work">Work</Link></li>
-              <li className="home__links-item"><Link to="/projects">Projects</Link></li>
-              <li className="home__links-item"><Link to="/resume">Resume</Link></li>
+              <li className="home__links-item">
+                <Link className="link" to="/about">
+                  <span className="link-icon"><MdHelp /></span>
+                  <span className="link-text">About</span>
+                </Link>
+              </li>
+              <li className="home__links-item">
+                <Link className="link" to="/work">
+                  <span className="link-icon"><MdWork /></span>
+                  <span className="link-text">Work</span>
+                </Link>
+              </li>
+              <li className="home__links-item">
+                <Link className="link" to="/projects">
+                  <span className="link-icon"><MdCode /></span>
+                  <span className="link-text">Projects</span>
+                </Link>
+              </li>
+              <li className="home__links-item">
+                <Link className="link" to="/resume">
+                  <span className="link-icon"><MdDescription /></span>
+                  <span className="link-text">Resume</span>
+                </Link>
+              </li>
           </ul>
         </nav>
       </div>

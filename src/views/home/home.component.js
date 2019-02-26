d7f0f030404af619824a17a1011793a02f7a9b61
Jonathon Orsi <jonathon.orsi@gmail.com>
2019-02-25 21:48:11 -0500

    rough working demo of background transitions

--- src/views/home/home.component.js
+++ src/views/home/home.component.js
@@ -8,26 +8,22 @@ class HomeViewComponent extends Component {
   }
   render() {
     return (
-      <div className="home">
-        <header className="home__header h-100">
-              <div className="flex justify-content-center align-items-center flex-column h-100">
-                  <h1>
-                    <span className="jonathon">Jonathon</span>
-                    <span className="orsi">Orsi</span>
-                  </h1>
-                  <h2 className="text-center">
-                      Full-stack Developer
-                  </h2>
-                  <nav className="home__nav w-100 text-center">
-                    <ul className="home__links-list list-unstyled">
-                        <li className="home__links-item"><Link to="/about">About</Link></li>
-                        <li className="home__links-item"><Link to="/work">Work</Link></li>
-                        <li className="home__links-item"><Link to="/projects">Projects</Link></li>
-                        <li className="home__links-item"><Link to="/resume">Resume</Link></li>
-                    </ul>
-                  </nav>
-              </div>
-          </header>
+      <div className="home h-100 flex justify-content-center align-items-center flex-column h-100">
+        <h1>
+          <span className="jonathon">Jonathon</span>
+          <span className="orsi">Orsi</span>
+        </h1>
+        <h2 className="text-center">
+            Full-stack Developer
+        </h2>
+        <nav className="home__nav w-100 text-center">
+          <ul className="home__links-list list-unstyled">
+              <li className="home__links-item"><Link to="/about">About</Link></li>
+              <li className="home__links-item"><Link to="/work">Work</Link></li>
+              <li className="home__links-item"><Link to="/projects">Projects</Link></li>
+              <li className="home__links-item"><Link to="/resume">Resume</Link></li>
+          </ul>
+        </nav>
       </div>
     );
   }

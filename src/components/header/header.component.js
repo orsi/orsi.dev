ea3ca02548d159af78ef7b3a986db63358a5cf3b
jorsi <jonathon.orsi@gmail.com>
2019-09-29 15:13:14 -0400

    simplify header styles

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -5,41 +5,38 @@ import { MdHelp, MdWork, MdCode, MdDescription } from 'react-icons/md';
 export class HeaderComponent extends Component {
   render() {
     return (
-      <div className="home h-100 flex justify-content-center align-items-center flex-column h-100">
-        <div className="home-title">
-          <h1>
-            <span className="jonathon">Jonathon</span>
-            <span className="orsi">Orsi</span>
-          </h1>
+      <div className="home">
+        <div className="container">
+          <h1 className="home-title">Jonathon Orsi</h1>
+          <nav className="home-nav">
+            <ul className="home-links-list list-unstyled">
+                <li className="home-links-item">
+                  <a className="link link--about" href="#about">
+                    <span className="link-icon"><MdHelp /></span>
+                    <span className="link-text">About</span>
+                  </a>
+                </li>
+                <li className="home-links-item">
+                  <a className="link link--work" href="#work">
+                    <span className="link-icon"><MdWork /></span>
+                    <span className="link-text">Work</span>
+                  </a>
+                </li>
+                <li className="home-links-item">
+                  <a className="link link--projects" href="#projects">
+                    <span className="link-icon"><MdCode /></span>
+                    <span className="link-text">Projects</span>
+                  </a>
+                </li>
+                <li className="home-links-item">
+                  <a className="link link--resume" href="#resume">
+                    <span className="link-icon"><MdDescription /></span>
+                    <span className="link-text">Resume</span>
+                  </a>
+                </li>
+            </ul>
+          </nav>
         </div>
-        <nav className="home__nav">
-          <ul className="home__links-list list-unstyled">
-              <li className="home__links-item">
-                <a className="link link--about" href="#about">
-                  <span className="link-icon"><MdHelp /></span>
-                  <span className="link-text">About</span>
-                </a>
-              </li>
-              <li className="home__links-item">
-                <a className="link link--work" href="#work">
-                  <span className="link-icon"><MdWork /></span>
-                  <span className="link-text">Work</span>
-                </a>
-              </li>
-              <li className="home__links-item">
-                <a className="link link--projects" href="#projects">
-                  <span className="link-icon"><MdCode /></span>
-                  <span className="link-text">Projects</span>
-                </a>
-              </li>
-              <li className="home__links-item">
-                <a className="link link--resume" href="#resume">
-                  <span className="link-icon"><MdDescription /></span>
-                  <span className="link-text">Resume</span>
-                </a>
-              </li>
-          </ul>
-        </nav>
       </div>
     );
   }

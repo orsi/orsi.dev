2fdb6d87caabaa00a06a97a6ad21afb3c2537970
jorsi <jonathon.orsi@gmail.com>
2019-09-29 16:14:15 -0400

    remove humble roots from projects and improve styling

--- src/components/projects/projects.component.js
+++ src/components/projects/projects.component.js
@@ -10,14 +10,13 @@ export class ProjectsComponent extends Component {
         return (
             <div className="projects">
                 <div className="container">
-                    <h2 className="projects-title">Projects</h2>
+                    <h2>Projects</h2>
 
                     <div className="project project--betical">
 
-                        <h3><a href="http://screencanadianelit.ca/betical/generate_one/">Betical</a></h3>
-
+                        <h4><a href="http://screencanadianelit.ca/betical/generate_one/">Betical</a></h4>
                         <div className="project-description">
-                            <p>A born-digital paragraph generator composed with remixed typed assemblage-letters by Dani Spinosa of <a href="https://genericpronoun.com/">Generic Pronoun</a>. Dani came to me with a set of typed letters she had created with the idea of a randomly generated "paragraph". Using the letter images with a combination of javascript and jQuery, Betical generates random combinations of letters into words, and words into paragraphs, to give the impression of a familiar language.</p>
+                            <p>A born-digital paragraph generator composed with remixed typed assemblage-letters by Dani Spinosa of <a href="https://genericpronoun.com/">Generic Pronoun</a> and myself. Using Dani's letters with a combination of javascript and jQuery, Betical generates random combinations of letters into words, and words into paragraphs, to give the impression of a familiar language.</p>
                         </div>
                         <div className="project-image-container">
                             <bet-ical></bet-ical>
@@ -25,43 +24,17 @@ export class ProjectsComponent extends Component {
 
                     </div>
 
-                    <div className="project project--humble-roots-media">
-                        <h3><a href="http://humblerootsmedia.com/">Humble Roots Media</a></h3>
-                        <div className="project-description">
-                            <p>Humble Roots Media, born from Adrian Miller and Myles Herod, is a digital content creator for businesses and corporations. They wanted a redesign of their website to better reflect their business personality and needed someone who could work with them to implement their vision. Built on Wordpress, I worked with both Adrian and Myles on a new theme template and built an api to deliver and display their beautiful cinemagraphs front and center.</p>
-
-                        </div>
-
-                        <div className="project-image-container">
-                            <img src={imageHumbleRootsMedia}
-                                alt="Humble Roots Media"
-                                className="project-image" />
-                        </div>
-
-                    </div>
-
                     <div className="project project--zen-html">
-                        <h3><a href="http://github.com/jorsi/zen-html">Zen HTML</a></h3>
-
-                        <div className="column w-m-66">
-                            <p>Zen HTML is a small, quick, and simple library for creating Custom Web Elements through ES5 tagged string literals. Heavily inspired by <a href="https://lit-html.polymer-project.org/">lit-html</a> and <a href="https://viperhtml.js.org/hyper.html">hyperHTML</a>, Zen HTML is a learning project I've built to better understand new Javascript features, npm, custom web elements, and overall project development, maintainance, and publishing.</p>
+                        <h4><a href="http://github.com/jorsi/zen-html">Zen HTML</a></h4>
+                        <div className="project-description">
+                            <p>Heavily inspired by <a href="https://lit-html.polymer-project.org/">lit-html</a> and <a href="https://viperhtml.js.org/hyper.html">hyperHTML</a>, Zen HTML is a small, quick, and simple library for creating Custom Web Elements through ES5 tagged string literals.  I built this as a learning project to better understand custom web elements and project publishing on npm.</p>
                         </div>
-
-                        <div className="project-image-container">
-                            <img className="project-image" alt="zenHTML" src={imageZenHtml} />
-                        </div>
-
                     </div>
 
                     <div className="project project--reverie">
-                        <h3><a href="http://github.com/jorsi/reverie">Reverie</a></h3>
-
+                        <h4><a href="http://github.com/jorsi/reverie">Reverie</a></h4>
                         <div className="project-description">
-                            <p className="special-margin-bottom">Reverie is an experiment in every sense. Fascinated by procedural generation and emerging properties, Reverie is my attempt at building a world simulation entirely with Node.js and the browser.</p>
-                        </div>
-
-                        <div className="project-image-container">
-                            <img className="project-image" alt="Reverie" src={imageReverie} />
+                            <p>Reverie is a disc-world generator written with Nodejs. After being fascinated by the immense complexity of <a href="http://www.bay12games.com/dwarves/">Dwarf Fortress</a>, I wanted to experiment with world generation myself.</p>
                         </div>
                     </div>
                 </div>

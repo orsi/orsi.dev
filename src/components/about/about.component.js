279530544c2fb190a52846bcaab7bae208cbb076
jorsi <jonathon.orsi@gmail.com>
2020-01-29 19:17:14 -0500

    remove Bombardier reference and add currently section

--- src/components/about/about.component.js
+++ src/components/about/about.component.js
@@ -18,8 +18,13 @@ export class AboutComponent extends Component {
                 <div className="container">
                     <div className="about-description">
                         <h2 className="about-title">About</h2>
-                        <p>Hello, my name is Jonathon Orsi and I am a software developer. I gravitate towards web technologies and front-end user experience. I currently work as a senior software developer at Bombardier Aerospace transitioning business workflow processes into web applications.</p>
+                        <p><em>Hello!</em> I am Jonathon Orsi, a software developer in Toronto, Canada. I specialize in web development and user experience.</p>
                         <p>I often experiment with <a href="https://www.webcomponents.org/introduction">custom web components</a> and have a deep interest in using the browser as a platform for <a href="https://github.com/jorsi/reverie/tree/develop">experimenting</a>. When I'm not coding I am usually playing <a href="https://www.youtube.com/watch?v=eyLexd5MCCw&feature=youtu.be&t=14">some</a> <a href="https://www.youtube.com/watch?v=zOHUfhNQsoc&feature=youtu.be&t=1231">sort</a> of <a href="http://jojogun.ca">instrument</a>.</p>
+
+                    </div>
+
+                    <div className="currently">
+                        I am currently looking for new work and taking digital media design courses at OCAD University.
                     </div>
                 </div>
             </div>

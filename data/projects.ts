8044eab1fa7ead95d1c7a4e654ed1ddce1e6128f
Jonathon Orsi <jonathon.orsi@gmail.com>
2021-11-20 14:09:46 -0500

    refactor into index file - project small enough to justify it

---
+++ data/projects.ts
@@ -0,0 +1,58 @@
+export default [
+  {
+    "title": "Chip-8C",
+    "href": "https://github.com/orsi/chip8c",
+    "imageSrc": "/chip8c.gif",
+    "tech": ["C/C++", "SDL2"],
+  },
+  {
+    "title": "Jojo Gun & the Bullets",
+    "href": "https://jojogun.ca",
+    "imageSrc": "/project-jojogun.gif",
+    "tech": ["HTML", "CSS", "Javascript", "SASS", "React"],
+  },
+  {
+    "id": "a-dark-room",
+    "title": "A Dark Room",
+    "href": "https://orsi.github.io/adarkroom",
+    "imageSrc": "/adarkroom.jpg",
+    "tech": ["Rust", "Python", "Typescript"],
+  },
+  {
+    "title": "Code X",
+    "href": "https://code-x.live",
+    "imageSrc": "/project-code-x.gif",
+  },
+  {
+    "id": "deno",
+    "title": "Deno",
+    "href":
+      "https://github.com/denoland/deno/commit/045e74bb39d7743b774cfd2b889bc6ce1e1ad245",
+    "imageSrc": "/deno-logo.svg",
+    "isSvg": true,
+    "tech": ["Rust", "Python", "Typescript"],
+  },
+  {
+    "id": "zen-html",
+    "title": "zenHTML",
+    "href": "https://github.com/orsi/zen-html",
+    "imageSrc": "/zen-html-logo.svg",
+    "tech": ["Typescript", ""],
+  },
+  {
+    "title": "Betical",
+    "href": "https://orsi.dev/betical/",
+    "imageSrc": "/project-betical.gif",
+  },
+  {
+    "title": "Humble Roots Media",
+    "href": "http://humblerootsmedia.com",
+    "imageSrc": "/project-humble-roots-media.gif",
+  },
+  {
+    "title": "UltimaXNA",
+    "href": "https://github.com/ZaneDubya/UltimaXNA",
+    "imageSrc": "/ultima-xna-logo.png",
+    "tech": ["C#", "XNA Framework"],
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,141 @@
-import '~/style/index.css';
-import { useDeno } from 'aleph/react'
-import React from 'react'
-import * as FaviconService from '~/services/favicon.service.ts';
-import Automata from '~/components/Automata.tsx';
-import DeveloperView from '~/components/DeveloperView.tsx';
+import "~/style/index.css";
+import React from "react";
+import * as FaviconService from "~/services/favicon.service.ts";
+import { AutomataService } from "~/services/automata.service.ts";
+import styles from "~/style/classes.module.css";
+import projects from "~/data/projects.ts";
 
-export default function Home() {
-  const version = useDeno(() => Deno.version.deno)
+export default function Index() {
+  const automataRef = React.useRef<HTMLDivElement>(null);
+  let automataService: AutomataService | null = null;
+  React.useEffect(() => {
+    if (automataRef.current !== null) {
+      automataService = new AutomataService(
+        automataRef.current,
+        true,
+      );
+      automataService.start();
+    }
+
+    return () => {
+      automataService?.destroy();
+      automataService = null;
+    };
+  }, [automataRef]);
 
   React.useEffect(() => {
     FaviconService.start();
   });
-  
+
   return (
     <>
-    <head>
+      <head>
         <title>Jonathon Orsi</title>
         <meta name="description" content="Jonathon Orsi's Homepage." />
         <meta name="keywords" content="alephjs,react,framework,deno" />
       </head>
-    <main>
-      <div id="background">
-        <Automata></Automata>
+      <div
+        ref={automataRef}
+        style={{
+          position: "absolute",
+          top: "0",
+          bottom: "0",
+          left: "0",
+          right: "0",
+          zIndex: 0,
+        }}
+      >
       </div>
-      <div>
-        <header>
-          <h1>Jonathon Orsi</h1>
-          <ul>
-            <li>
-              <a href="https://github.com/orsi" target="_blank" rel="noreferrer">
+      <main
+        style={{
+          position: "relative",
+          zIndex: 1,
+        }}
+      >
+        <div>
+          <header>
+            <h1>Jonathon Orsi</h1>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/orsi"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/jonorsi/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                </a>
+              </li>
+              <li>
+                <a href="mailto:jonathon.orsi@gmail.com">
+                </a>
+              </li>
+            </ul>
+          </header>
+          <div
+            id="about"
+            style={{
+              marginTop: "32px",
+            }}
+          >
+            <h2>About Me</h2>
+            <p>
+              I design and develop web applications, digital art showcases, and
+              experimental tools. I have 7 years of professional experience
+              working with{" "}
+              <a href="https://thrillworks.com" target="_blank">
+                Thrillworks
               </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/jonorsi/" target="_blank" rel="noreferrer">
+              ,{" "}
+              <a href="https://bombardier.com" target="_blank">
+                Bombardier Inc.
               </a>
-            </li>
-            <li>
-              <a href="mailto:jonathon.orsi@gmail.com">
+              , and freelancing.
+            </p>
+            <p>
+              When I am not programming, I compose music, design sound, mix, and
+              master audio for all media. You may also see me performing in{" "}
+              <a target="_blank" href="https://jojogun.ca">
+                JoJo Gun & the Bullets
               </a>
-            </li>
-          </ul>
-        </header>
-        <div
-          id="about"
-          style={{
-            marginTop: '32px',
-          }}
-        >
-          <h2>About Me</h2>
-          <p>
-            I design and develop web applications, digital art showcases, and
-            experimental tools. I have 7 years of professional experience working with{' '}
-            <a href="https://thrillworks.com" target="_blank">
-              Thrillworks
-            </a>
-            ,{' '}
-            <a href="https://bombardier.com" target="_blank">
-              Bombardier Inc.
-            </a>
-            , and freelancing.
-          </p>
-          <p>
-            When I am not programming, I compose music, design sound, mix, and
-            master audio for all media. You may also see me performing in{' '}
-            <a target="_blank" href="https://jojogun.ca">
-              JoJo Gun & the Bullets
-            </a>
-            . You can listen to my original music and compositions on{' '}
-            <a target="_blank" href="https://orsi.bandcamp.com/">
-              bandcamp.
-            </a>
-          </p>
-          <p>I live in Toronto, Ontario, Canada.</p>
+              . You can listen to my original music and compositions on{" "}
+              <a target="_blank" href="https://orsi.bandcamp.com/">
+                bandcamp.
+              </a>
+            </p>
+            <p>I live in Toronto, Ontario, Canada.</p>
+          </div>
+
+          <div
+            id="development"
+            style={{
+              marginTop: "32px",
+            }}
+          >
+            <h2>View Work</h2>
+
+            {projects && (
+              <ul className={styles.test}>
+                {projects.map((project, i) => {
+                  return (
+                    <li key={i} id={project.id}>
+                      <a href={project.href} target="_blank">
+                        <img src={project.imageSrc} />
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
         </div>
-        <DeveloperView />
-      </div>
-    </main>
+      </main>
     </>
-    
-  )
+  );
 }

620c9ee2329975434ba061e813ba147888a161d3
Jonathon Orsi <jonathon.orsi@gmail.com>
2022-08-29 22:27:02 -0400

    add gen file

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,13 +3,13 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/index.tsx";
-import * as $1 from "./routes/privacy.tsx";
+import * as $1 from "./routes/jons-tuner.tsx";
 import * as $$0 from "./islands/Background.tsx";
 
 const manifest = {
   routes: {
     "./routes/index.tsx": $0,
-    "./routes/privacy.tsx": $1,
+    "./routes/jons-tuner.tsx": $1,
   },
   islands: {
     "./islands/Background.tsx": $$0,

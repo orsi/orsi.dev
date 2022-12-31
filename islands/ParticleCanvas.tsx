5b7c18883f97ad204e5875098d4869ace19565f1
Jonathon Orsi <jonathon.orsi@gmail.com>
2022-12-31 15:31:26 -0500

    add glitch effect

--- islands/ParticleCanvas.tsx
+++ islands/ParticleCanvas.tsx
@@ -2,10 +2,10 @@ import { useCallback, useEffect, useRef } from "preact/hooks";
 import * as THREE from "three";
 import { FontLoader } from "three/addons/loaders/FontLoader.js";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { FlyControls } from "three/addons/controls/FlyControls.js";
-
-const MAX_SIZE = 500;
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { GlitchPass } from "three/addons/postprocessing/GlitchPass.js";
 
 export default function ParticleCanvas() {
   const state = useRef<any>({
@@ -16,6 +16,8 @@ export default function ParticleCanvas() {
     height: 0,
     width: 0,
     camera: undefined,
+    composer: undefined,
+    glitchPass: undefined,
     renderer: undefined,
     scene: new THREE.Scene(),
     geometry: new THREE.BufferGeometry(),
@@ -39,7 +41,7 @@ export default function ParticleCanvas() {
         0.1,
         10000
       );
-      state.current.camera.position.set(90, 70, 100);
+      state.current.camera.position.set(0, 25, 50);
       state.current.camera.lookAt(0, 0, 0);
 
       // renderer
@@ -49,15 +51,15 @@ export default function ParticleCanvas() {
       state.current.renderer.autoClear = false;
       node.appendChild(state.current.renderer.domElement);
 
-      // grid
-      const size = 50;
-      const divisions = 50;
-      const gridHelper = new THREE.GridHelper(size, divisions);
-      state.current.scene.add(gridHelper);
+      // // grid
+      // const size = 50;
+      // const divisions = 50;
+      // const gridHelper = new THREE.GridHelper(size, divisions);
+      // state.current.scene.add(gridHelper);
 
-      // axes
-      const axesHelper = new THREE.AxesHelper(50);
-      state.current.scene.add(axesHelper);
+      // // axes
+      // const axesHelper = new THREE.AxesHelper(50);
+      // state.current.scene.add(axesHelper);
 
       // orbit control
       state.current.controls = new FlyControls(
@@ -97,60 +99,49 @@ export default function ParticleCanvas() {
       );
 
       // spheres and stuff
-      for (let i = 0; i < 500; i++) {
-        const x = Math.random() * MAX_SIZE - MAX_SIZE / 2;
-        const y = Math.random() * MAX_SIZE - MAX_SIZE / 2;
-        const z = Math.random() * MAX_SIZE - MAX_SIZE / 2;
-        const randomShape = Math.random();
-        let geometry;
-        if (randomShape < 0.33) {
-          geometry = new THREE.WireframeGeometry(
-            new THREE.BoxGeometry(1, 1, 1)
-          );
-        } else if (randomShape < 0.66) {
-          geometry = new THREE.WireframeGeometry(
-            new THREE.SphereGeometry(1, 5, 5)
-          );
-        } else {
-          const shape = new THREE.Shape();
-          const x = 0;
-          const y = 0;
+      let geometry = new THREE.BoxGeometry(1, 1, 1);
+      let material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true,
+      });
+      let object = new THREE.Mesh(geometry, material);
+      object.position.set(-10, 20, 40);
+      state.current.scene.add(object);
+      state.current.objects.push(object);
 
-          shape.moveTo(x - 2, x - 2);
-          shape.lineTo(2, -2);
-          shape.lineTo(0, 2);
-          geometry = new THREE.ShapeGeometry(shape);
-        }
+      geometry = new THREE.SphereGeometry(1, 5, 5);
+      material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true,
+      });
+      object = new THREE.Mesh(geometry, material);
+      object.position.set(0, 20, 40);
+      state.current.scene.add(object);
+      state.current.objects.push(object);
 
-        const randomColor = Math.random();
-        const color =
-          randomColor < 0.33
-            ? 0x00ffa0
-            : randomColor < 0.66
-            ? 0xff00a0
-            : 0x00a0ff;
-
-        const randomObject = Math.random();
-        const object =
-          randomObject < 0.5
-            ? new THREE.Mesh(
-                geometry,
-                new THREE.MeshBasicMaterial({ color, side: THREE.DoubleSide })
-              )
-            : new THREE.Line(geometry, new THREE.LineBasicMaterial({ color }));
-        object.position.set(x, y, z);
-        state.current.scene.add(object);
-        state.current.objects.push(object);
-      }
+      geometry = new THREE.ConeGeometry(1, 1, 3);
+      material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true,
+      });
+      object = new THREE.Mesh(geometry, material);
+      object.position.set(10, 20, 40);
+      state.current.scene.add(object);
+      state.current.objects.push(object);
 
       // dat gui
       const dat = await import("https://esm.sh/dat.gui@0.7.9");
       const gui = new dat.GUI();
-      const cameraFolder = gui.addFolder("Camera");
-      cameraFolder.add(state.current.camera.position, "x", -500, 500);
-      cameraFolder.add(state.current.camera.position, "y", -500, 500);
-      cameraFolder.add(state.current.camera.position, "z", -500, 500);
-      cameraFolder.open();
+
+      // postprocessing
+
+      state.current.composer = new EffectComposer(state.current.renderer);
+      state.current.composer.addPass(
+        new RenderPass(state.current.scene, state.current.camera)
+      );
+
+      state.current.glitchPass = new GlitchPass();
+      state.current.composer.addPass(state.current.glitchPass);
 
       // update
       animate();
@@ -160,25 +151,15 @@ export default function ParticleCanvas() {
   const animate = () => {
     const delta = clock.getDelta();
 
-    // update positions
+    // rotate objects
     for (let i = 0; i < state.current.objects.length; i++) {
-      const randomPositionX = Math.random() * 2 - 1;
-      const randomPositionY = Math.random() * 2 - 1;
-      const randomPositionZ = Math.random() * 2 - 1;
       const object = state.current.objects[i];
-      object.position.x += randomPositionX;
-      object.position.y += randomPositionY;
-      object.position.z += randomPositionZ;
-    }
-
-    if (state.current.text) {
-      state.current.text.rotation.y += 0.01;
-      state.current.text.rotation.x += 0.01;
-      state.current.text.rotation.z += 0.01;
+      object.rotation.x += 0.01;
+      object.rotation.y += 0.01;
     }
 
     state.current.controls.update(delta);
-    state.current.renderer.render(state.current.scene, state.current.camera);
+    state.current.composer.render();
 
     requestAnimationFrame(animate);
   };

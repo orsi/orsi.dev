99eed46b20053dec2d6a7a67d2527adfc6a33cd1
Jonathon Orsi <jonathon.orsi@gmail.com>
2023-01-07 15:43:24 -0500

    update title

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,7 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Ultra</title>
+        <title>ORSI</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href={useAsset("/favicon.ico")} />
         <link rel="stylesheet" href={useAsset("/style.css")} />

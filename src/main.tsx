4526a7081835619e9f4bcc06d98b82edc3b0ffce
orsi <jonathon.orsi@gmail.com>
2025-09-18 15:43:36 -0400

    chore: cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,23 @@
+import './global.css';
 import { createRoot } from 'jinx';
 
-createRoot(document.querySelector<HTMLDivElement>('#app')!).render(
-  <main>
-    <h1>a-bit-distorted</h1>
-    <p>hello...</p>
-  </main>
-);
+function App() {
+  return (
+    <main
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '8px',
+        margin: '0 auto',
+        maxWidth: '900px',
+        padding: '0 16px',
+      }}
+    >
+      <h1>a bit distorted</h1>
+      <p>hello...</p>
+    </main>
+  );
+}
+
+createRoot(document.querySelector<HTMLDivElement>('body')!).render(<App />);

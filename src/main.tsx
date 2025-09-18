0974d649623e098569a84f7ba1c0033fbe4a3b68
orsi <jonathon.orsi@gmail.com>
2025-09-18 15:26:17 -0400

    feat(wip): test out jinx with git submodule

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'jinx';
+
+createRoot(document.querySelector<HTMLDivElement>('#app')!).render(
+  <main>
+    <h1>a-bit-distorted</h1>
+    <p>hello...</p>
+  </main>
+);
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  esbuild: {
+    jsx: 'transform',
+    jsxFactory: '__jsx',
+    jsxFragment: '__Fragment',
+    jsxInject: "import { jsx as __jsx, Fragment as __Fragment } from 'jinx';",
+  },
+  resolve: {
+    alias: {
+      jinx: path.resolve(__dirname, './vendor/jinx/packages/jinx'),
+    },
+  },
+});

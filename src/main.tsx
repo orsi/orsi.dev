26fdc2a43e21bd19b6d7c5cd2427fa6c3432ab2f
orsi <jonathon.orsi@gmail.com>
2025-11-23 15:00:48 -0500

    chore: update commit

--- src/main.tsx
+++ src/main.tsx
@@ -31,43 +31,65 @@ const Button2: JSX.Function = ({ children }) => {
 // render([1, 'hi', false], document.querySelector('main')!);
 // render(<Button2>push me!</Button2>, document.querySelector('main')!);
 
-const test = (
-  <>
-    <h1 style={{ color: 'green' }}>title</h1>
-    <Button2>push me!</Button2>
-    {true && <marquee>true shortcircuit!</marquee>}
-    {false && <marquee>false shortcircuit!</marquee>}
-    {true ? <span>true tern!</span> : <span>false tern!</span>}
-    {false ? <span>true tern!</span> : <span>false tern!</span>}
-    {true && true}
-    {false && false}
-  </>
-);
+// const test = (
+//   <>
+//     <h1 style={{ color: 'green' }}>title</h1>
+//     <Button2>push me!</Button2>
+//     {true && <marquee>true shortcircuit!</marquee>}
+//     {false && <marquee>false shortcircuit!</marquee>}
+//     {true ? <span>true tern!</span> : <span>false tern!</span>}
+//     {false ? <span>true tern!</span> : <span>false tern!</span>}
+//     {true && true}
+//     {false && false}
+//   </>
+// );
 // document.querySelector<HTMLDivElement>('body')!.append(test);
+const CArray = () => {
+  return [1, 'hi', false ? <span>true</span> : <span>false</span>];
+};
 
-const Test = () => {
-  const [count, setCount] = useState(0);
+const CString = () => {
+  return 'hi';
+};
+
+const CNumber = () => {
+  return 4;
+};
+
+const CBool = () => {
+  return true;
+};
+
+const Count = ({ count }: { count: number }) => {
+  return <div id="count">{count}</div>;
+};
+
+const Counter = ({ count }: { count: number }) => {
   return (
     <>
-      <button onClick={() => setCount((value) => ++value)}>
-        Count: {count}
-      </button>
-      {count % 2 === 0 && <small>EVEN!</small>}
+      <Count count={count} />
+      {count % 2 === 0 ? (
+        <small id="even">EVEN!</small>
+      ) : (
+        <small id="odd">ODD!</small>
+      )}
     </>
   );
 };
-document.querySelector<HTMLDivElement>('body')!.append(
-  <main>
-    <h1>Testing</h1>
-    <Test />
-    <ul>
-      <>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </>
-    </ul>
-  </main>
-);
-// render(test, document.querySelector('main')!);
+
+const MyComponent = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button onClick={() => setCount((value) => ++value)}>Increase</button>
+      <Counter count={count} />
+      <CArray />
+      <CString />
+      <CNumber />
+      <CBool />
+    </>
+  );
+};
+
+const result = <MyComponent />;
+document.querySelector<HTMLDivElement>('body')!.append(result);
